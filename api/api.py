1d82928fac469464515e67a9e8887dcdf933faac
Locke Birdsey <locke.birdsey@gmail.com>
2021-02-06 12:56:32 +0200

    update flask app

--- api/api.py
+++ api/api.py
@@ -8,6 +8,9 @@ app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 task_repo = TaskRepository()
 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/tasks')
 def show_tasks():
