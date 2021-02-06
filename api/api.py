54c8d7668711239a7e740792ddf92826101b3fd9
Locke Birdsey <locke.birdsey@gmail.com>
2021-02-06 12:53:50 +0200

    update flask app

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 
 from task_repo import TaskRepository
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 task_repo = TaskRepository()
 
